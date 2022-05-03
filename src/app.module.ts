20c0d71aba622f995e2be4eb13b7f2523386a983
STNeto1 <netofreitas7052@gmail.com>
2022-05-03 08:38:01 -0300

    feat: graphql with mercurius

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,8 @@ import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
 import { LoggerModule } from 'nestjs-pino'
 import { MikroOrmModule } from '@mikro-orm/nestjs'
+import { GraphQLModule } from '@nestjs/graphql'
+import { MercuriusDriver, MercuriusDriverConfig } from '@nestjs/mercurius'
 
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
@@ -14,7 +16,11 @@ import { configValidationSchema } from './configuration/config'
       validationSchema: configValidationSchema
     }),
     LoggerModule.forRoot(),
-    MikroOrmModule.forRoot()
+    MikroOrmModule.forRoot(),
+    GraphQLModule.forRoot<MercuriusDriverConfig>({
+      driver: MercuriusDriver,
+      graphiql: true
+    })
   ],
   controllers: [AppController],
   providers: [AppService]
