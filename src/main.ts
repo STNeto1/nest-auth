489291dddd5defca6fe81a1a189f7448d8565bee
STNeto1 <netofreitas7052@gmail.com>
2022-05-03 08:04:01 -0300

    feat: fastify

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,21 @@
 import { NestFactory } from '@nestjs/core'
 import { ValidationPipe } from '@nestjs/common'
+import {
+  FastifyAdapter,
+  NestFastifyApplication
+} from '@nestjs/platform-fastify'
 
 import { AppModule } from './app.module'
 
 export async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter()
+  )
 
   app.useGlobalPipes(new ValidationPipe())
 
-  await app.listen(3000)
+  await app.listen(3000, '0.0.0.0')
 
   return app
 }
