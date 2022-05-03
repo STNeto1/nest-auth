de626a0e5c594b7d492822e0bc039d3579e67c53
STNeto1 <netofreitas7052@gmail.com>
2022-05-03 07:48:48 -0300

    feat: config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import config from './configuration/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: config,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
