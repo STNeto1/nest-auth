342f3342f3f3d31cdcb19821fb2701f97ad04ba6
STNeto1 <netofreitas7052@gmail.com>
2022-05-03 22:05:51 -0300

    test: auth resolver

--- src/auth/auth.resolver.spec.ts
+++ src/auth/auth.resolver.spec.ts
@@ -1,12 +1,24 @@
 import { Test, TestingModule } from '@nestjs/testing'
 import { AuthResolver } from './auth.resolver'
+import { createMock } from '@golevelup/ts-jest'
+import { AuthService } from './auth.service'
 
 describe('AuthResolver', () => {
   let resolver: AuthResolver
 
-  beforeEach(async () => {
+  const mockedUserService = createMock<AuthService>({
+    validateUser: jest.fn().mockResolvedValue({ access_token: 'jwt' })
+  })
+
+  beforeAll(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthResolver]
+      providers: [
+        AuthResolver,
+        {
+          provide: AuthService,
+          useValue: mockedUserService
+        }
+      ]
     }).compile()
 
     resolver = module.get<AuthResolver>(AuthResolver)
@@ -14,5 +26,16 @@ describe('AuthResolver', () => {
 
   it('should be defined', () => {
     expect(resolver).toBeDefined()
+  })
+
+  describe('login', () => {
+    it('should return a access token for login', async () => {
+      const result = await resolver.login({
+        email: 'mail@mail.com',
+        password: '102030'
+      })
+
+      expect(result.access_token).toEqual('jwt')
+    })
   })
 })
