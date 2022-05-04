a5254e12fedd8897f94f01f01a10827b2b86dabe
STNeto1 <netofreitas7052@gmail.com>
2022-05-03 22:36:24 -0300

    test: user resolver

--- src/user/user.resolver.spec.ts
+++ src/user/user.resolver.spec.ts
@@ -1,13 +1,30 @@
 import { Test, TestingModule } from '@nestjs/testing'
+import { createMock } from '@golevelup/ts-jest'
+
 import { UserResolver } from './user.resolver'
 import { UserService } from './user.service'
+import { User } from './entities/user.entity'
 
 describe('UserResolver', () => {
   let resolver: UserResolver
 
-  beforeEach(async () => {
+  const userStub: User = {
+    id: 1,
+    name: 'user',
+    email: 'mail@mail.com',
+    password: 'password',
+    createdAt: new Date(),
+    updatedAt: new Date()
+  }
+
+  const mockedUserService = createMock<UserService>()
+
+  beforeAll(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UserResolver, UserService]
+      providers: [
+        UserResolver,
+        { provide: UserService, useValue: mockedUserService }
+      ]
     }).compile()
 
     resolver = module.get<UserResolver>(UserResolver)
@@ -15,5 +32,56 @@ describe('UserResolver', () => {
 
   it('should be defined', () => {
     expect(resolver).toBeDefined()
+  })
+
+  describe('createUser', () => {
+    it('should create a user', async () => {
+      const result = await resolver.createUser({
+        name: 'name',
+        email: 'mail@mail.com',
+        password: '102030'
+      })
+
+      expect(result).toEqual(true)
+      expect(mockedUserService.create).toHaveBeenCalled()
+    })
+  })
+
+  describe('findAll', () => {
+    it('should find all users', async () => {
+      mockedUserService.findAll.mockResolvedValue([userStub])
+
+      const result = await resolver.findAll()
+
+      expect(result).toEqual([userStub])
+    })
+  })
+
+  describe('findOne', () => {
+    it('should find a user', async () => {
+      mockedUserService.findOne.mockResolvedValue(userStub)
+
+      const result = await resolver.findOne(1)
+
+      expect(result).toStrictEqual(userStub)
+    })
+  })
+
+  describe('updateUser', () => {
+    it('should update a user', async () => {
+      const result = await resolver.updateUser(userStub, {})
+
+      expect(result).toEqual(true)
+      expect(mockedUserService.update).toHaveBeenCalled()
+    })
+  })
+
+  describe('deleteUser', () => {
+    it('should delete a user', async () => {
+      const result = await resolver.removeUser(userStub)
+
+      expect(result).toEqual(true)
+      expect(mockedUserService.remove).toHaveBeenCalled()
+    })
   })
 })
