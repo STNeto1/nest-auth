019491e8ae3fc0b22955fe05e88815f3d0f3c914
STNeto1 <netofreitas7052@gmail.com>
2022-05-03 22:39:37 -0300

    test: coverage

--- src/auth/auth.resolver.spec.ts
+++ src/auth/auth.resolver.spec.ts
@@ -2,9 +2,19 @@ import { Test, TestingModule } from '@nestjs/testing'
 import { AuthResolver } from './auth.resolver'
 import { createMock } from '@golevelup/ts-jest'
 import { AuthService } from './auth.service'
+import { User } from '../user/entities/user.entity'
 
 describe('AuthResolver', () => {
   let resolver: AuthResolver
+
+  const userStub: User = {
+    id: 1,
+    name: 'user',
+    email: 'mail@mail.com',
+    password: 'password',
+    createdAt: new Date(),
+    updatedAt: new Date()
+  }
 
   const mockedUserService = createMock<AuthService>({
     validateUser: jest.fn().mockResolvedValue({ access_token: 'jwt' })
@@ -35,7 +45,17 @@ describe('AuthResolver', () => {
         password: '102030'
       })
 
-      expect(result.access_token).toEqual('jwt')
+      expect(result).toStrictEqual({
+        access_token: 'jwt'
+      })
+    })
+  })
+
+  describe('whoAmi', () => {
+    it('should return logged user', async () => {
+      const result = await resolver.whoAmi(userStub)
+
+      expect(result).toStrictEqual(userStub)
     })
   })
 })
